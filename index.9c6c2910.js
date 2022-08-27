b50eb6f15e3c860bdababe195c0150fea96f7768
OlgaKonotop <107248128+OlgaKonotop@users.noreply.github.com>
2022-08-27 19:22:31 +0000

    Deploying to gh-pages from @ OlgaKonotop/GooD_IT@2f896bf63c8ef94c7dd124454715e67e9f1ae320 🚀

---
+++ index.9c6c2910.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu-close]"),n=document.querySelector("[data-menu]");function c(){n.classList.toggle("nav--is-open")}e.addEventListener("click",c),t.addEventListener("click",c)})();
+//# sourceMappingURL=index.9c6c2910.js.map
---
+++ index.d5752b62.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-open]"),n=document.querySelector("[data-menu-close]"),t=document.querySelector("[data-menu]");function c(){t.classList.toggle("nav--is-open")}e.addEventListener("click",c),n.addEventListener("click",c)}();
+//# sourceMappingURL=index.d5752b62.js.map
