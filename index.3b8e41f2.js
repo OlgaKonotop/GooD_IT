c1dc71194d5e5ea9a56011eae776655b82f979a6
OlgaKonotop <107248128+OlgaKonotop@users.noreply.github.com>
2022-08-29 14:31:31 +0000

    Deploying to gh-pages from @ OlgaKonotop/GooD_IT@2966fa12365e2deed85c93f65be4061dcbf6f116 🚀

---
+++ index.3b8e41f2.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]")};function n(){e.modal.classList.toggle("nav--is-open")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)})();new Swiper(".swiper-container",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.3b8e41f2.js.map
---
+++ index.f6e86b24.js
@@ -0,0 +1,2 @@
+var Swipes=new Swiper(".swiper-container",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.f6e86b24.js.map
